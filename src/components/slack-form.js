8b3358f434454bf9609929d91000e295738e361b
Ebonie Butler <ebonie@yikesinc.com>
2020-03-16 10:24:26 -0400

    Slack sign up styled

--- src/components/slack-form.js
+++ src/components/slack-form.js
@@ -6,26 +6,30 @@ const SlackForm = () => (
     <form name="slack-sign-up" method="post" data-netlify="true">
       <input type="hidden" name="form-name" value="slack-sign-up" />
       <label className={ styles.label }>
-        Name
-        <input type="text" name="name" id="name" />
+        <p>Name</p>
+        <input type="text" name="name" id="name" required/>
       </label>
       <label className={ styles.label }>
-        Email
-        <input type="email" name="email" id="email" />
+        <p>Email</p>
+        <input type="email" name="email" id="email" required/>
       </label>
       <label className={ styles.label }>
-        Twitter Handle
-        <input type="text" name="twitter" id="twitter" />
+        <p>Twitter Handle <small>(if applicable)</small></p>
+        <input type="text" name="twitter" id="twitter"/>
       </label>
       <label className={ styles.label }>
-        How do you use WordPress?
-        <textarea name="wordpresslove" id="wordpresslove" rows="5" />
+        <p>Wordpress.org Username <small>(if applicable)</small></p>
+        <input type="text" name="wporg" id="wporg"/>
       </label>
       <label className={ styles.label }>
-        Preferred Slack Username
-        <input type="text" name="slackusername" id="slackusername" />
+        <p>How do you use WordPress?</p>
+        <textarea className={ styles.textarea } name="wordpresslove" id="wordpresslove" rows="5" required/>
       </label>
-      <button type="submit">Send</button>
+      <label className={ styles.label }>
+        <p>Preferred Slack Username</p>
+        <input type="text" name="slackusername" id="slackusername" required/>
+      </label>
+      <button className={ styles.submit } type="submit">Send</button>
     </form>
   </div>
 )
